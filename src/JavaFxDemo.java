49e76429b7c6d155c964d93e0b36141691b07b41
joniketo <joni.m.keto@gmail.com>
2018-04-18 15:34:15 +0300

    eka commit

---
+++ src/JavaFxDemo.java
@@ -0,0 +1,54 @@
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+
+public class JavaFxDemo extends Application  {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Some title");
+
+        button = new Button();
+        button.setText("click me!");
+        
+        // This class will handle the button events
+        button.setOnAction(e -> System.out.println("JuuJuu"));
+        
+        
+        
+//        button.setOnAction(new EventHandler<ActionEvent>() {
+//        	@Override
+//        	public void handle(ActionEvent event) {
+//        		System.out.println("JeeJee!");
+//        	}
+//		});
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+//    @Override
+//    public void handle(ActionEvent event) {
+//        if (event.getSource() == button) {
+//            System.out.println("JAAHAS, nyt tapahtuu jotain!");
+//        }
+        
+    
+}
